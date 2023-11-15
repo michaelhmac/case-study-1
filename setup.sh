90e0a825286ca41f3834490dd2050cc8e9146c08
michaelhmac <67394941+michaelhmac@users.noreply.github.com>
2023-11-15 15:29:20 -0800

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash 
+#
+# Setup file to install update GIT and install apache2 
+# Will Git Clone my repo and mv file under /var/www/html location 
+
+echo "+++ Upating GIT +++"
+echo 
+sudo apt update git -y 
+echo 
+echo "+++ Installing Apache2 +++"
+echo 
+sudo apt install apache2 -y
+echo 
+echo "+++ Enabling Apache on Startup +++" 
+echo 
+sudo systemctl enable apache2
+echo 
+echo "+++ GIT Clone Repository +++"
+echo 
+cd /var/www/html
+sudo git clone http:https://github.com/michaelhmac/case-study-1/ . 
+echo 
+echo "+++ Checking if web page is up +++" 
+URL=$(curl -s http://checkip.amazonaws.com)
+timeout_seconds=10
+
+if curl --output /dev/dull --silent --head --fail --max-time $timeout_seconds "$URL"; then 
+    echo "The web page is up." 
+else 
+    echo "The web page is down or unreachable." 
+fi
+
+
