c9d74089dc593dd145720176886d95d368dcc32a
michaelhmac <67394941+michaelhmac@users.noreply.github.com>
2023-11-15 16:10:20 -0800

    Update setup.sh
    updated check web page section.

--- setup.sh
+++ setup.sh
@@ -5,28 +5,33 @@
 
 echo "+++ Upating GIT +++"
 echo 
-sudo apt update git -y 
+sudo apt update git 
 echo 
 echo "+++ Installing Apache2 +++"
 echo 
 sudo apt install apache2 -y
-echo 
-echo "+++ Enabling Apache on Startup +++" 
-echo 
-sudo systemctl enable apache2
 echo 
 echo "+++ Moivng all files and folder to /var/www/html +++"
 echo 
 sudo mv * .git /var/www/html
 echo 
 echo "+++ Checking if web page is up +++" 
-URL=$(curl -s http://checkip.amazonaws.com)
-timeout_seconds=10
 
-if curl --output /dev/dull --silent --head --fail --max-time $timeout_seconds "$URL"; then 
-    echo "The web page is up." 
-else 
-    echo "The web page is down or unreachable." 
+url=$(curl -s http://checkip.amazonaws.com)
+timeout_seconds=5
+
+response_code=$(curl --output /dev/null --silent --head --fail --max-time $timeout_seconds -w "%{http_code}" "$url")
+
+if [ "$response_code" -eq 200 ]; then
+    echo "The web page is up (HTTP 200 OK)."
+else
+    echo "The web page is down or unreachable (HTTP $response_code)."
 fi
+
+
+
+
+
+
 
 
